eab55c605403323fe0089b29c08ec124905f8851
msandulache <mariussandulache2015@gmail.com>
2024-07-05 04:04:08 +0300

    fara Memcached

--- config/cache.php
+++ config/cache.php
@@ -51,30 +51,30 @@ return [
             'lock_path' => storage_path('framework/cache/data'),
         ],
 
-//        'memcached' => [
-//            'driver' => 'memcached',
-//            'persistent_id' => env('MEMCACHED_PERSISTENT_ID'),
-//            'sasl' => [
-//                env('MEMCACHED_USERNAME'),
-//                env('MEMCACHED_PASSWORD'),
-//            ],
-//            'options' => [
-//                // Memcached::OPT_CONNECT_TIMEOUT => 2000,
-//            ],
-//            'servers' => [
-//                [
-//                    'host' => env('MEMCACHED_HOST', '127.0.0.1'),
-//                    'port' => env('MEMCACHED_PORT', 11211),
-//                    'weight' => 100,
-//                ],
-//            ],
-//        ],
-//
-//        'redis' => [
-//            'driver' => 'redis',
-//            'connection' => env('REDIS_CACHE_CONNECTION', 'cache'),
-//            'lock_connection' => env('REDIS_CACHE_LOCK_CONNECTION', 'default'),
-//        ],
+        'memcached' => [
+            'driver' => 'memcached',
+            'persistent_id' => env('MEMCACHED_PERSISTENT_ID'),
+            'sasl' => [
+                env('MEMCACHED_USERNAME'),
+                env('MEMCACHED_PASSWORD'),
+            ],
+            'options' => [
+                // Memcached::OPT_CONNECT_TIMEOUT => 2000,
+            ],
+            'servers' => [
+                [
+                    'host' => env('MEMCACHED_HOST', '127.0.0.1'),
+                    'port' => env('MEMCACHED_PORT', 11211),
+                    'weight' => 100,
+                ],
+            ],
+        ],
+
+        'redis' => [
+            'driver' => 'redis',
+            'connection' => env('REDIS_CACHE_CONNECTION', 'cache'),
+            'lock_connection' => env('REDIS_CACHE_LOCK_CONNECTION', 'default'),
+        ],
 
         'dynamodb' => [
             'driver' => 'dynamodb',
